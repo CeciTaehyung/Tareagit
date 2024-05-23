d67835c48fab14b3826f66b800d772bbebdb8be3
gabri777Des <gabrisac2006@gmail.com>
2024-05-22 21:43:53 -0600

    Ahora los usuarios pueden agregar datos

--- src/EjemploEmcapsulamiento.java
+++ src/EjemploEmcapsulamiento.java
@@ -1,11 +1,36 @@
+import java.util.Scanner;
+
 public class EjemploEmcapsulamiento {
     public  static void main (String[]args){
         Estudiantes obj = new Estudiantes();
-        obj.setNombre("Cecita");
-        obj.setApellido("Arbizu");
-        obj.setEdad(18);
-        obj.setPeso(111);
-        obj.setGenero("M");
+        Scanner po = new Scanner(System.in);
+
+        String a1, a2, a3;
+        int b; double c;
+
+        System.out.println("Escribe la informacion del libro.");
+
+        System.out.println("Tu Nombre: ");
+        a1 = po.nextLine();
+        obj.setNombre(a1);
+
+        System.out.println("Tu apellido: ");
+        a2 = po.nextLine();
+        obj.setApellido(a2);
+
+        System.out.println("Tu Edad: ");
+        b = po.nextInt();
+        obj.setEdad(b);
+
+        System.out.println("Tu peso: ");
+        c = po.nextDouble();
+        obj.setPeso(c);
+
+        System.out.println("Tu genero: ");
+        a3 = po.next();
+        obj.setGenero(a3);
+
+        System.out.println();
 
         System.out.println("Name: " + obj.getNombre());
         System.out.println("Apellido: " + obj.getApellido());
